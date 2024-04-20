6f4b2c73fddae6ae3a59847745d5983ca4b918c7
Lukas <l-sauter@web.de>
2024-04-20 20:14:44 +0200

    feat: asteroids despawn and tweaks

--- scenes/entities/asteroid.gd
+++ scenes/entities/asteroid.gd
@@ -14,6 +14,7 @@ var DIRECTION
 var SPEED
 var HEALTH
 var CHILDREN_COUNT
+var ROTATION
 
 
 static func create_asteroid(size: int, direction: Vector2) -> Asteroid:
@@ -24,20 +25,23 @@ static func create_asteroid(size: int, direction: Vector2) -> Asteroid:
 		asteroid.scale = Vector2(0.2, 0.2)
 		asteroid.SIZE = SMALL
 		asteroid.SPEED = 600 * speed_randomizer
-		asteroid.HEALTH = 1
+		asteroid.HEALTH = 2
 		asteroid.CHILDREN_COUNT = 0
+		asteroid.ROTATION = randf_range(-0.1, 0.1)
 	elif (size == MEDIUM):
 		asteroid.scale = Vector2(0.4, 0.4)
 		asteroid.SIZE = MEDIUM
-		asteroid.SPEED = 400 * speed_randomizer
-		asteroid.HEALTH = 2
+		asteroid.SPEED = 450 * speed_randomizer
+		asteroid.HEALTH = 3
 		asteroid.CHILDREN_COUNT = 3
+		asteroid.ROTATION = randf_range(-0.05, 0.05)
 	else: 
 		asteroid.scale = Vector2(0.6, 0.6)
 		asteroid.SIZE = LARGE
-		asteroid.SPEED = 200 * speed_randomizer
-		asteroid.HEALTH = 4
-		asteroid.CHILDREN_COUNT = 3
+		asteroid.SPEED = 300 * speed_randomizer
+		asteroid.HEALTH = 5
+		asteroid.CHILDREN_COUNT = 2
+		asteroid.ROTATION = randf_range(-0.01, 0.01)
 	asteroid.DIRECTION = direction
 	return asteroid
 
@@ -49,20 +53,22 @@ func _ready():
 
 func _process(delta):
 	position = position + DIRECTION * SPEED * delta
-	for object in get_overlapping_bodies():
-		object.hit(1)
-		HEALTH = -1
-	if HEALTH <= 0:
-		spawn_children()
-		queue_free()
+	rotate(ROTATION)
 
 
 func spawn_children():
 	for i in CHILDREN_COUNT:
-		var child = Asteroid.create_asteroid(SIZE-1, Vector2.RIGHT + Vector2(0, randf_range(-0.6, 0.6)))
+		var child = Asteroid.create_asteroid(SIZE-1, Vector2.RIGHT + Vector2(0, randf_range(-0.8, 0.8)))
 		child.position = self.position
 		get_parent().add_child(child)
 
 
 func hit(damage):
 	HEALTH -= damage
+	if HEALTH <= 0:
+		spawn_children()
+		queue_free()
+
+
+func on_screen_exited():
+	queue_free()
