1bdbeb16a27f52560abf59eb425c22e1d865c4c5
Lukas <l-sauter@web.de>
2024-04-20 14:24:45 +0200

    feat: attacker can shoot

--- scenes/entities/projectile.gd
+++ scenes/entities/projectile.gd
@@ -1,14 +1,21 @@
 extends Area2D
 
 
+var GLOBALS
+
+
 var DAMAGE = 1
 var SPEED = 1000
 var DRAWN = false
 
 
+func _ready():
+	GLOBALS = get_node("/root/Globals")
+
+
 func _physics_process(delta):
-	var direction = Vector2.RIGHT
-	position += direction * SPEED * delta
+	var direction = Vector2.LEFT
+	position += direction * SPEED * delta * GLOBALS.SPEED_SCALE
 	
 
 func on_screen_exited():
